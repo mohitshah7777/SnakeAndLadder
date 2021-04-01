3880cb35d6f82d313fef7f188a45552ff892896a
Mohit Shah <mohitshah7777@gmail.com>
2021-04-01 21:17:39 +0530

    [Mohit] initialized player position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,6 +2,9 @@ public class SnakesAndLadder {
 
 	public static void main(String[] args)
 	{
-
+		System.out.println("--------------------------------------");
+		System.out.println("Welcome to Snakes and Ladder game");
+		System.out.println("--------------------------------------\n");
+		int Player1_Position=0;
 	}
 }
